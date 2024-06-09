930c60b5943e77a493554678efaffc92e8e8fe0a
ufsuleiman <ufsuleiman.ug@atbu.edu.ng>
2024-06-09 21:13:16 +0100

    my first commit

---
+++ week4_PLP.py
@@ -0,0 +1,16 @@
+class person:
+    def __init__(self):
+        self.name='Suleiman umar farouk'
+        self.age=23
+        self.gender='male'
+    def talk(self):
+        print('My name is',self.name,',' 'i am', self.age, 'years old', ',''i am a', self.gender)
+          
+
+obj1=person()
+obj1.talk()
+
+
+     
+        
+    
